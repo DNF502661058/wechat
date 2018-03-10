89fea96822896a440ec4dd6af3f92b4f315239d8
DNF502661058 <502661058@qq.com>
2018-03-10 14:44:23 +0800

    wechat Signed-off-by: DNF502661058 <502661058@qq.com>

---
+++ wechat.cfg.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @Author: jsy135135
+ * @email:732677288@qq.com
+ * @Date:   2018-03-08 14:50:45
+ * @Last Modified by:   jsy135135
+ * @Last Modified time: 2018-03-09 10:43:07
+ */
+define("TOKEN", "chunwan");
+define("APPID", "wx674bedd1fbbac608");
+define("APPSECRET", "e7b81cfce9ee5321a86d27113b478f17");
